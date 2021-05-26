1ebae35108a7e387d487615ed207518c181885ce
王志献 <3150828420@qq.com>
2021-05-26 11:54:53 +0800

    5-26

---
+++ 2102/5-26/table.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<table border="5px">
+<thead>
+<th>姓名</th>
+<th>年龄</th>
+<th>Email</th>
+</thead>
+    <?php 
+    $list = [
+        ["name"=>"张三","age"=>11,"email"=>"zhangsan@qq.com"],
+        ["name"=>"李四","age"=>22,"email"=>"lisi@qq.com"],
+        ["name"=>"王五","age"=>33,"email"=>"wangwu@qq.com"],
+        ["name"=>"赵六","age"=>44,"email"=>"zhaoliu@qq.com"],
+    ];
+  
+   foreach($list as $k=>$v){
+       echo "<tr>"."<td>".$v['name']."</td>"."<td>".$v['age']."</td>"."<td>".$v['email']."</td>"."</tr>";
+   }
+
+
+
+?>
+    
+    
+    
+    
+    </table>
+</body>
+</html>
